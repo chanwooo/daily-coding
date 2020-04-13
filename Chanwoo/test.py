22d26b7d69088a025ab647af4f80bc60e1990769
ChanwooKim <chanwookim@me.com>
2020-04-14 04:07:12 +0900

    add code test field

--- Chanwoo/test.py
+++ Chanwoo/test.py
@@ -1,52 +1,52 @@
-# # graph = {
-# #     'A': ['B'],
-# #     'B': ['A', 'C', 'H'],
-# #     'C': ['B', 'D'],
-# #     'D': ['C', 'E', 'G'],
-# #     'E': ['D', 'F'],
-# #     'F': ['E'],
-# #     'G': ['D'],
-# #     'H': ['B', 'I', 'J', 'M'],
-# #     'I': ['H'],
-# #     'J': ['H', 'K'],
-# #     'K': ['J', 'L'],
-# #     'L': ['K'],
-# #     'M': ['H']
-# # }
-# #
-# # print(graph)
-# #
-# # def bfs(graph, start_node):
-# #     visit = list()
-# #     queue = list()
-# #     queue.append(start_node)
-# #
-# #     while queue:
-# #         node = queue.pop(0)
-# #         if node not in visit:
-# #             visit.append(node)
-# #             queue.extend(graph[node])
-# #             print(queue)
-# #     return visit
-# #
-# # def dfs(graph, start_node):
-# #     visit = list()
-# #     stack = list()
-# #
-# #     stack.append(start_node)
-# #     while stack:
-# #         node = stack.pop()
-# #         if node not in visit:
-# #             visit.append(node)
-# #             # stack.extend(graph[node])
-# #             stack.extend(reversed(graph[node]))
-# #     return visit
-# #
-# # print(bfs(graph,'A'))
-# #
-# # print(dfs(graph,'A'))
-# #
-# #
+# graph = {
+#     'A': ['B'],
+#     'B': ['A', 'C', 'H'],
+#     'C': ['B', 'D'],
+#     'D': ['C', 'E', 'G'],
+#     'E': ['D', 'F'],
+#     'F': ['E'],
+#     'G': ['D'],
+#     'H': ['B', 'I', 'J', 'M'],
+#     'I': ['H'],
+#     'J': ['H', 'K'],
+#     'K': ['J', 'L'],
+#     'L': ['K'],
+#     'M': ['H']
+# }
+#
+# print(graph)
+#
+# def bfs(graph, start_node):
+#     visit = list()
+#     queue = list()
+#     queue.append(start_node)
+#
+#     while queue:
+#         node = queue.pop(0)
+#         if node not in visit:
+#             visit.append(node)
+#             queue.extend(graph[node])
+#             print(queue)
+#     return visit
+#
+# def dfs(graph, start_node):
+#     visit = list()
+#     stack = list()
+#
+#     stack.append(start_node)
+#     while stack:
+#         node = stack.pop()
+#         if node not in visit:
+#             visit.append(node)
+#             # stack.extend(graph[node])
+#             stack.extend(reversed(graph[node]))
+#     return visit
+#
+# print(bfs(graph,'A'))
+#
+# print(dfs(graph,'A'))
+#
+#
 #
 # def solution(N, number):
 #     arr = [[], [], [], [], [], [], [], [], []]
@@ -77,7 +77,12 @@
 # for a in arr:
 #     print(sorted(list(set(a))))
 import heapq
-heap = [100,2,3,4,5,6,10]
+
+heap = [100, 2, 3, 4, 5, 6, 10]
 heapq.heapify(heap)
 
-print(heap) # [1,2,3,5,7]
+print(heap)  # [1,2,3,5,7]
+print('aaa')
+
+for h in heap:
+    print(h)
