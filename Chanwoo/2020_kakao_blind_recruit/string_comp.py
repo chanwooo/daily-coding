8544ee88ccad85b59a909d9da4d6815548a6f3fb
ChanwooKim <chanwookim@me.com>
2020-04-24 07:27:56 +0900

    [Programmers] 60057 - 문자열 압축
    https://programmers.co.kr/learn/courses/30/lessons/60057
    2020 카카오 공채 코딩테스트 기출

--- Chanwoo/2020_kakao_blind_recruit/string_comp.py
+++ Chanwoo/2020_kakao_blind_recruit/string_comp.py
@@ -4,44 +4,44 @@
 https://programmers.co.kr/learn/courses/30/lessons/60057
 2020 카카오 공채 코딩테스트 기출
 """
+
+
 def solution(s):
     L = len(s)
     result = []
 
-
     if L == 1:
         return 1
 
-    for unit in range(1, L//2+1):
+    for unit in range(1, L // 2 + 1):
         comp_str = ""
 
         count = 1
 
         print()
-        print("unit :",unit)
+        print("unit :", unit)
 
         for i in range(unit, L + unit, unit):
             # print(i,i+unit,i-unit,i, s[i:i+unit], s[i-unit:i])
 
-            print(">", i, s[i:i+unit], s[i-unit:i], count)
-            if s[i:i+unit] == s[i-unit:i]:
+            print(">", i, s[i:i + unit], s[i - unit:i], count)
+            if s[i:i + unit] == s[i - unit:i]:
                 count += 1
             else:
                 if count > 1:
-                    print(s[i-unit:i], count)
-                    comp_str += str(count)+s[i-unit:i]
+                    print(s[i - unit:i], count)
+                    comp_str += str(count) + s[i - unit:i]
                     count = 1
                 else:
-                    print(s[i-unit:i])
+                    print(s[i - unit:i])
                     comp_str += s[i - unit:i]
                 # print(s[i:i+unit])
                 # print('comp',count)
-        print(i,"?")
+        print(i, "?")
         print(comp_str)
         result.append(len(comp_str))
     print(result)
     return min(result)
-
 
 
 # s = "aaaba"  # 2
