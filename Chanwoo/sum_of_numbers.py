ef61bdcc5e2e5ebe9a07de5c8531e7a520e1db84
ChanwooKim <chanwookim@me.com>
2020-05-19 23:15:01 +0900

    [BOJ] 2003 - 수들의 합 2
    https://www.acmicpc.net/problem/2003

--- Chanwoo/sum_of_numbers.py
+++ Chanwoo/sum_of_numbers.py
@@ -6,12 +6,12 @@ https://www.acmicpc.net/problem/2003
 #
 # N, M = map(int, "4 2".split(' '))
 # A = list(map(int, "1 1 1 1".split(' ')))
-#
-# N, M = map(int, "10 5".split(' '))
-# A = list(map(int, "1 2 3 4 2 5 3 1 1 2".split(' ')))
 
-N, M = map(int, input().split(' '))
-A = list(map(int, input().split()))
+N, M = map(int, "10 5".split(' '))
+A = list(map(int, "1 2 3 4 2 5 3 1 1 2".split(' ')))
+#
+# N, M = map(int, input().split(' '))
+# A = list(map(int, input().split()))
 
 start = 0
 end = 0
@@ -19,18 +19,17 @@ cur = 0
 count = 0
 
 while True:
-    # print(start, end, count, A[start:end+1], sum(A[start:end+1]))
-    if start > N or end >= N:
+    if end >= N:
         break
+    # print(start, end, count, A[start:end+1], sum(A[start:end+1]))
 
     cur = sum(A[start:end + 1])
 
     if cur > M:
         start += 1
-    elif cur < M:
-        end += 1
     else:
-        count += 1
+        if cur == M:
+            count += 1
         end += 1
 
 print(count)
