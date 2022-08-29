489cdf36782a84b06b4f6af6c0356c54568bfeec
Chanwookim <chanwookim@me.com>
2022-08-29 17:48:16 +0900

    dict 사용해서 해결해보기

--- Python3/2022t_n/tn_example.py
+++ Python3/2022t_n/tn_example.py
@@ -1,43 +1,41 @@
 def solution(servers, sticky, requests):
     answer = [[] for _ in range(servers)]
-    next_server = 0
+    curr_server = 0
+    req_to_server_id = {}
 
     for i, req in enumerate(requests):
-        print("====req", req, answer)
+        print(answer, req_to_server_id)
 
-        if sticky:
-            find = False
-            # find
-            for server_id in range(servers):
-                if req in answer[server_id]:
-                    answer[server_id].append(req)
-                    next_server = (server_id + 1) % servers
-                    find = True
-                    print("-----find", req, server_id, next_server)
-
-            # not find, add
-            if not find:
-                print("-not find", req, server_id, next_server)
-                answer[next_server].append(req)
-                next_server = (next_server + 1) % servers
-
-        else:
+        if not sticky:
             answer[i % servers].append(req)
+        else:
+            if req in req_to_server_id:
+                answer[req_to_server_id[req]].append(req)
+
+            else:
+                req_to_server_id[req] = curr_server
+                answer[curr_server].append(req)
+                curr_server = (curr_server+1) % servers
 
     return answer
 
+#
+# servers = 2
+# sticky = False
+# requests = [1, 2, 3, 4]
+# result = solution(servers, sticky, requests)
+# print(result)
 
-servers = 2
-sticky = False
-requests = [1, 2, 3, 4]
-result = solution(servers, sticky, requests)
-print(result)
+# #########
+# servers = 2
+# sticky = True
+# requests = [1, 2, 3, 4]
+# result = solution(servers, sticky, requests)
+# print(result)
+# print(solution(2, True, [1,1,2,2]))
 
-##########
-servers = 2
-sticky = True
-requests = [1, 2, 3, 4]
-result = solution(servers, sticky, requests)
-print(result)
-
-print(solution(3, True, [1, 1, 3, 2, 1, 2, 3, 1]))
+# print(solution(3, True, [1, 1, 3, 2, 1, 2, 3, 1]))
+#
+#
+print(solution(3, True, [1,1,2,1,3,3,4]))
+# 1114,2,33
