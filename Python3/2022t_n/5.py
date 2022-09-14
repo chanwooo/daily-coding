96652e47ccdbed405b54cba90bf2082de6b5024f
Chanwookim <chanwookim@me.com>
2022-09-15 02:54:20 +0900

    list 사용해서 해결해보기

--- Python3/2022t_n/5.py
+++ Python3/2022t_n/5.py
@@ -26,12 +26,38 @@ def solution(tasks):
     return answer
 
 
-solution([1, 1, 2, 3, 3, 2, 2])  # 3
-solution([4, 1, 1, 1, 1, 2, 3])  # -1
-solution([1, 1, 2, 2])  # 2 -> 2 2
-solution([1, 1, 1, 1, 1, 2, 2])  # 3 -> 3 2 2
-solution([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # 4 -> 3 3 3 2
-solution([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # 5
-solution([1 for _ in range(111)])
+def solution_using_list(tasks):
+    answer = 0
+    count = 0
+
+    tasks.sort()
+    tasks.append("-")
+
+    for i in range(1, len(tasks)):
+        count += 1
+
+        if tasks[i - 1] != tasks[i]:
+            if count == 1:
+                return -1
+
+            if count % 3 == 0:
+                answer += count // 3
+            else:
+                answer += count // 3 + 1
+
+            count = 0
+
+    return answer
+
+
+print(solution_using_list([1, 1, 2, 3, 3, 2, 2]))
+
+# solution([1, 1, 2, 3, 3, 2, 2])  # 3
+# solution([4, 1, 1, 1, 1, 2, 3])  # -1
+# solution([1, 1, 2, 2])  # 2 -> 2 2
+# solution([1, 1, 1, 1, 1, 2, 2])  # 3 -> 3 2 2
+# solution([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # 4 -> 3 3 3 2
+# solution([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # 5
+# solution([1 for _ in range(111)]) # 37
 
 # 2,3으로 만들수있는 수 중에 가장 작은거
